fdcbf498650d5389029f61542d763ad47fef3934
Tao <werewaswas@live.com>
2018-07-27 06:16:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,13 +7,18 @@ class Triangle
   end
 
   def kind
+    @a > 0 && @b > 0 && @c > 0
 
+    begin
+      raise TriangleError
+    rescue TriangleError => error
+      error.message
+    end
   end
 
   class TriangleError < StandardError
     def message
       puts "not a valid triangle"
-
     end
   end
 end
